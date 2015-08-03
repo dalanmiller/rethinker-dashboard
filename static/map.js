9c286e6b792050edfed4fb6c61f79962b2dc2a6f
Daniel Alan Miller <dalanmiller@rethinkdb.com>
2015-08-03 16:31:52 -0400

    u

--- static/map.js
+++ static/map.js
@@ -3,9 +3,8 @@
 ContribMap = Ember.Application.create();
 
 ContribMap.Router.map(function(){
-  this.resource('map', { path: '/' });
-  this.resource('users');
-  this.resource("user", { path: ":user_id"})
+  // this.resource('map', { path: '/' });
+  this.resource('users', {path :"/"});
 });
 
 ContribMap.MapController = Ember.Controller.extend({
@@ -19,77 +18,103 @@ ContribMap.Users = DS.Model.extend({
   coords: DS.attr('string'),
 });
 
-// App.PostAdapter = DS.RESTAdapter.extend({
-//   namespace: 'api/v1'
-// });
-
-ContribMap.UserAdapter = DS.RESTAdapter.extend({
-  namespace: "api"
-})
-
 ContribMap.UsersAdapter = DS.RESTAdapter.extend({
   namespace: "api"
 })
 
-
 ContribMap.UsersRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('users');
-  }
-});
-
-ContribMap.UserRoute = Ember.Route.extend({
-  model: function(params){
-    return this.store.find("users", params.post_id);
-  }
-})
-
-ContribMap.MapRoute = Ember.Route.extend({
   model: function(){
     return this.store.findAll("users");
   },
-  setupController: function() {
-    console.log("INDEXROUTE SETUPCONTROLLER");
-    // var users = ContribMap.User.find();
-    var users = [];
-    this.controllerFor('map').set('users', users);
+  setupController: function(controller, model) {
+    console.log("INDEXROUTE SETUPCONTROLLER", model);
+    controller.set("model", model);
   }
 });
 
-ContribMap.LeafletMapComponent = Ember.Component.extend({
-  attributeBindings: ['style'],
+// ContribMap.LeafletMapComponent = Ember.Component.extend({
+//   attributeBindings: ['style'],
+//
+//   width: 'auto',
+//   height: '600px',
+//   latitude: 0,
+//   longitude: 0,
+//   zoom: 1,
+//
+//   style: function() {
+//     console.log("STYLE");
+//     console.log(this);
+//     return [
+//       'width:' + this.get('width'),
+//       'height:' + this.get('height')
+//     ].join(';');
+//   }.property('width', 'height'),
+//
+//   setView: function() {
+//     var map    = this.get('map'),
+//   			center = [this.get('latitude'), this.get('longitude')],
+// 				zoom   = this.get('zoom');
+//
+// 		map.setView(center, zoom);
+//   }.observes('latitude', 'longitude', 'zoom'),
+//
+//   didInsertElement: function() {
+//     var map = L.map(this.get('element'));
+//
+//     var users = this.get("users");
+//
+//     console.log("users");
+//     console.log(users);
+//
+//     console.log(this.model);
+//
+//     this.set('map', map);
+//
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//       maxZoom: 18,
+//       id: 'dalanmiller.008a9358',
+//       accessToken: "pk.eyJ1IjoiZGFsYW5taWxsZXIiLCJhIjoiZDg0MWY0MjMzZjE4Y2VkOGE4NGY1ZTI5ZTg5MjUzMTMifQ.ylhul8dwba3XpBQDOkTK6w"
+//     }).addTo(map);
+//
+//     this.setView();
+//
+//     map.on('move', this.mapDidMove, this);
+//
+//     map.invalidateSize();
+//   },
+//
+//   // willRemoveElement: function() {
+//   //   var map = this.get('map');
+//   //   if (map) map.remove();
+//   // },
+//   //
+//   mapDidMove: function() {
+//     var map    = this.get('map'),
+//         center = map.getCenter(),
+//         zoom   = map.getZoom();
+//
+//     this.setProperties({
+//       latitude: center.lat,
+//       longitude: center.lng,
+//       zoom: zoom
+//     });
+//   }
+// });
 
-  width: 'auto',
-  height: '600px',
-  latitude: 0,
-  longitude: 0,
-  zoom: 1,
-
-  style: function() {
-    console.log("STYLE");
-    console.log(this);
-    return [
-      'width:' + this.get('width'),
-      'height:' + this.get('height')
-    ].join(';');
-  }.property('width', 'height'),
-
-  setView: function() {
-    var map    = this.get('map'),
-  			center = [this.get('latitude'), this.get('longitude')],
-				zoom   = this.get('zoom');
-
-		map.setView(center, zoom);
-  }.observes('latitude', 'longitude', 'zoom'),
+ContribMap.UsersView = Ember.View.extend({
+  templateName: "users",
 
   didInsertElement: function() {
-    var map = L.map(this.get('element'));
-
+    var view = this,
+        map = L.map(this.$('#map').get(0));
+    // var map = L.map(this.get('element
+    console.log(this.get("controller.model"));
+    console.log(map);
     var users = this.get("users");
 
     console.log("users");
     console.log(users);
-
     console.log(this.model);
 
     this.set('map', map);
@@ -106,22 +131,5 @@ ContribMap.LeafletMapComponent = Ember.Component.extend({
     map.on('move', this.mapDidMove, this);
 
     map.invalidateSize();
-  },
-
-  // willRemoveElement: function() {
-  //   var map = this.get('map');
-  //   if (map) map.remove();
-  // },
-  //
-  mapDidMove: function() {
-    var map    = this.get('map'),
-        center = map.getCenter(),
-        zoom   = map.getZoom();
-
-    this.setProperties({
-      latitude: center.lat,
-      longitude: center.lng,
-      zoom: zoom
-    });
   }
 });
